e25de7b23e3a8edde3bf808c5d407da0e3c21dec
samarthprabhu2007-art <samarthprabhu2007@gmail.com>
2026-09-17 20:56:27 +0530

    added q

---
+++ Trees/Path_sum_in_tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool call(TreeNode* root, int targetSum) {
+        if(root==nullptr) return false;
+        if(root->left == nullptr  && root->right == nullptr){
+            return root->val == targetSum;
+        }
+        return call(root->left,targetSum - root->val) || call(root->right,targetSum - root->val);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==nullptr){
+            return false;
+        }
+        return call(root,targetSum);
+    }
+};
